a3c61f2b4965f0ac6c82f62672eac54e5f52f8f5
b1uuye <onibudoi87@gmail.com>
2025-08-12 10:21:59 +0100

    Add cors to API

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
-app.use(express.json()) //passes json so we can use it in our server side code
 
 const logger = require('./logger')
 const { fruitsRouter } = require('./routes/fruits')
 
+
+app.use(express.json()) //passes json so we can use it in our server side code
+app.use(cors())
 app.use(logger)
+app
 app.use('/fruits' , fruitsRouter)
 
 module.exports = {
